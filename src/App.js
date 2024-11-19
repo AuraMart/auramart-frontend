5594f5b85e60d4ada216c559672f690986ad3375
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-11-19 05:57:04 +0530

    fix(home): move assets folder to public folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import * as React from 'react';
-import { ProductList } from './pages/ProductList';
+import { ProductList } from './components/Product/ProductList';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 function App() {
   return (
 
     <div className="">
-      <ProductList />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/productList" element={<ProductList/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
