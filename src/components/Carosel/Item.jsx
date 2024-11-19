c4cfb3b188fa0986ddab6b0587d07d370bb3f6df
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-11-19 07:39:25 +0530

    feat(home): add new arrival section

--- src/components/Carosel/Item.jsx
+++ src/components/Carosel/Item.jsx
@@ -6,11 +6,37 @@ export const Item = (props) => {
       <img
         src={props.item.image}
         alt={props.item.title}
-        style={{ width: "100%", height: "90vh", position: "relative" }}
+        style={{ width: "100%", height: "90vh", position: "relative" , zIndex: "10"}}
       />
-      <h1 style={{ position: "absolute", top: "70%", left: "35%",color:'white', fontSize:'96px',fontFamily:''  }}>
+      <div style={{width:'100%', height:"20vh", position:"absolute",zIndex:"20",backgroundColor:"black",opacity:"0.7",bottom:"0"}}> </div>
+      <h1
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "35%",
+          color: "white",
+          fontSize: "96px",
+          fontFamily: "font-rusilla",
+          zIndex: "40",
+        }}
+      >
         Aura Mart
       </h1>
+      <h1
+        style={{
+          position: "absolute",
+          top: "75%",
+          left: "28%",
+          color: "white",
+          fontSize: "96px",
+          // fontFamily: "Basic Commercial",
+          zIndex: "40",
+        }}
+        className="font-mogra"
+      >
+        New Collection
+      </h1>
+     
     </Paper>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Carousel from "react-material-ui-carousel";
 import items from "../components/Carosel/carousel-items";
 import { Item } from "../components/Carosel/Item";
+import { Box } from "@mui/material";
+import { ProductList } from "../components/Product/ProductList";
 
 export const Home = () => {
   return (
@@ -11,6 +13,29 @@ export const Home = () => {
           <Item key={i} item={item} />
         ))}
       </Carousel>
+      {/* <Box
+      sx={{
+        height:"40vh",
+        width:"100%",
+        my: 2,
+        position: "relative",
+      }}
+    > */}
+      <h1
+        style={{
+          position: "absolute",
+          top: "0%",
+          left: "2%",
+          color: "black",
+          fontSize: "48px",
+          fontFamily: "",
+        }}
+      >
+        New Arrival
+      </h1>
+      <ProductList />
+      
+    {/* </Box> */}
 
     </div>
   );
