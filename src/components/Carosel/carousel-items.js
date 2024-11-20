f7bb1a9328f543123344afca6fd0e017153271de
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-11-20 16:33:15 +0530

    fix(home): change carousel images

--- src/components/Carosel/carousel-items.js
+++ src/components/Carosel/carousel-items.js
@@ -2,15 +2,15 @@
 var items = [
     {
         title: "shopping",
-        image: "/assets/images/carosel1.jpg"
+        image: "/assets/images/carosel1.png"
     },
     {
         title: "hand bags",
-        image: "/assets/images/carosel2.jpg"
+        image: "/assets/images/carosel2.png"
     },
     {
         title: "women clothes",
-        image: "/assets/images/carosel3.jpg"
+        image: "/assets/images/carosel3.png"
     }
 
 
