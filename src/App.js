7ba4e49b5f66bd0306dd7e6c16e0a1b9ffb6cd03
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-11-20 09:43:33 +0530

    feat(home): add footer initial commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import * as React from 'react';
 import { ProductList } from './components/Product/ProductList';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
+import FooterSection from './components/Footer/FooterSection';
 
 function App() {
   return (
 
-    <div className="">
+    <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/productList" element={<ProductList/>} />
         </Routes>
       </BrowserRouter>
+      <FooterSection />
     </div>
   );
 }
