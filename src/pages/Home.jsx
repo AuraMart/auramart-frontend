04836a4c008272288064381c6e405010b4ff02ec
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-11-20 10:08:31 +0530

    feat(home): add footer section second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import Carousel from "react-material-ui-carousel";
 import items from "../components/Carosel/carousel-items";
 import { Item } from "../components/Carosel/Item";
-import { Box } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Stack,
+  styled,
+} from "@mui/material";
 import { ProductList } from "../components/Product/ProductList";
 import { CategorySection } from "../components/Category/CategorySection";
 import { MenCategoryList } from "../components/Category/menCategoryList";
 import { WomenCategoryList } from "../components/Category/womenCategoryList";
+import { grey } from "@mui/material/colors";
+import { NewArrival } from "../components/NewArrival";
 
 export const Home = () => {
   return (
@@ -23,20 +32,11 @@ export const Home = () => {
           marginTop: "3%",
         }}
       >
-        <h1
-          style={{
-            textAlign: "left",
-            color: "black",
-            fontSize: "36px",
-            fontFamily: "",
-            marginLeft: "4%",
-            marginBottom: "2%",
-          }}
-        >
+        <h1 className="font-abril text-[36px] align-left text-gray-500 ml-[4%] mb-[2%]">
           New Arrival
         </h1>
-        <div style={{ display: "flex", justifyItems: "center" }}>
-          <ProductList />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <NewArrival />
         </div>
       </Box>
       {/* category types */}
@@ -46,16 +46,7 @@ export const Home = () => {
           marginTop: "3%",
         }}
       >
-        <h1
-          style={{
-            textAlign: "left",
-            color: "black",
-            fontSize: "36px",
-            fontFamily: "",
-            marginLeft: "4%",
-            marginBottom: "2%",
-          }}
-        >
+        <h1 className="font-abril text-[36px] align-left text-gray-500 ml-[4%] mb-[2%]">
           Shop By Category
         </h1>
         <CategorySection />
@@ -68,19 +59,10 @@ export const Home = () => {
           marginTop: "3%",
         }}
       >
-        <h1
-          style={{
-            textAlign: "left",
-            color: "black",
-            fontSize: "36px",
-            fontFamily: "",
-            marginLeft: "4%",
-            marginBottom: "2%",
-          }}
-        >
+        <h1 className="font-abril text-[36px] align-left text-gray-500 ml-[4%] mb-[2%]">
           Categories for Men
         </h1>
-        <div style={{display: "flex", justifyItems: "center" }}>
+        <div style={{ display: "flex", justifyItems: "center" }}>
           <MenCategoryList />
         </div>
       </Box>
@@ -90,25 +72,85 @@ export const Home = () => {
         sx={{
           width: "100%",
           marginTop: "3%",
-          
         }}
       >
-        <h1
-          style={{
-            textAlign: "left",
-            color: "black",
-            fontSize: "32px",
-            fontFamily: "",
-            marginLeft: "4%",
-            marginBottom: "2%",
-          }}
-        >
+        <h1 className="font-abril text-[36px] align-left text-gray-500 ml-[4%] mb-[2%]">
           Categories for Women
         </h1>
-        <div style={{display: "flex", justifyItems: "center" }}>
+        <div style={{ display: "flex", justifyItems: "center" }}>
           <WomenCategoryList />
         </div>
+      </Box>
+
+      {/* top brands */}
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <BlackBox>
+          <h5 className="absolute top-10 text-center text-5xl text-white font-abril">
+            Available Top Brands
+          </h5>
+          <h3 className="absolute top-28 text-center text-2xl text-yellow-400 font-mogra">
+           - Discover Premium Fashion Labels -
+          </h3>
+          <Stack
+            direction="row"
+            spacing={2}
+            sx={{ marginTop: "10%" }}
+            className="absolute bottom-8"
+          >
+            <Card className="rounded-full">
+              <CardMedia
+                image={"/assets/icons/polo.jpeg"}
+                className="h-20 w-40 "
+              />
+            </Card>
+            <Card className="rounded-full">
+              <CardMedia
+                image={"/assets/icons/HM.png"}
+                className="h-20 w-40 "
+              />
+            </Card>
+            <Card className="rounded-full">
+              <CardMedia
+                image={"/assets/icons/levis.png"}
+                className="h-20 w-40 "
+              />
+            </Card>
+            <Card className="rounded-full">
+              <CardMedia
+                image={"/assets/icons/puma.png"}
+                className="h-20 w-40 "
+              />
+            </Card>
+            <Card className="rounded-full h-20 w-40">
+              <CardMedia
+                image={"/assets/icons/nike.png"}
+                className="h-16 w-40 "
+              />
+            </Card>
+          </Stack>
+        </BlackBox>
       </Box>
     </div>
   );
 };
+
+const BlackBox = styled(Box)(({ theme }) => ({
+  color: theme.palette.getContrastText(grey[300]),
+  backgroundColor: grey[900],
+  opacity: "0.9",
+  marginTop: "4%",
+  borderRadius: "10px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "50vh",
+  width: "75%",
+  position: "relative",
+}));
