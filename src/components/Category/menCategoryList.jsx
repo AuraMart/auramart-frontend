950f5ca3e9c6ea18b9eafbc9636465f3e326c0ba
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-12-05 19:40:47 +0530

    feat(home): add men categories again

--- src/components/Category/menCategoryList.jsx
+++ src/components/Category/menCategoryList.jsx
@@ -1,26 +1,12 @@
 import React, { useRef, useEffect,useState } from "react";
-//import { men } from "./categories";
+import { men } from "./categories";
 import { Box } from "@mui/material";
 import { Categorycard } from "./CategoryCard";
 import { getProductsMen } from "../../Services/api";
 
 
 export const MenCategoryList = () => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    // Fetch products from the backend
-    const fetchProducts = async () => {
-      try {
-        const data = await getProductsMen();
-        console.log(data.url);
-        setProducts(data);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
-    };
 
-    fetchProducts();
-  }, []);
   return (
     <div style={{display:'flex',justifyContent:"stretch",alignContent:'center'}}>
     <Box
@@ -31,12 +17,11 @@ export const MenCategoryList = () => {
         justifyContent:"center",
       }}
     >
-      {products.map((men) => (
+      {men.map((men) => (
         <Categorycard
           key={men.id}
           title={men.title}
-          //image={men.url}
-          image={Array.isArray(men.imageUrls) ? men.imageUrls[0] : men.imageUrls}
+          image={men.image}
         />
       ))}
     </Box>
